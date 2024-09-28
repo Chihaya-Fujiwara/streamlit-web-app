03d4216c66c15ac1496173250f10e1c6d1cee5f3
chihaya-fujiwara <chihaya.fuji811@gmail.com>
2024-09-28 16:27:54 +0900

    5 commmit

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.write('aaaaaaaaaaaaaaaa')
+
+st.set_page_config(
+    page_title="About Me",
+    page_icon="atom_symbol", 
+    layout="wide",
+    #initial_sidebar_state="expanded",
+)
+
+
+def main():
+    st.title('welcome')
+
+if __name__ == '__main__':
+    main()
